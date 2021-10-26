cb56dfbc057982f99365de9f60ee54558328c87f
Samuel Sackey <smlscky@gmail.com>
2021-10-26 20:12:58 +0000

    Add topbar to Homescreen

--- screens/Home.js
+++ screens/Home.js
@@ -1,18 +1,32 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { Feather, FontAwesome } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 export default function Home() {
   return (
-    <View style={styles.page}>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={styles.page}>
+      <View style={styles.topbar}>
+        <TouchableOpacity>
+          <Feather name="menu" size={24} color="black" />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <FontAwesome name="opencart" size={24} color="black" />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   page: {
     flex: 1,
+    paddingHorizontal: 10,
+  },
+  topbar: {
+    paddingTop: 40,
+    flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center",
-    justifyContent: "center",
   },
 });
