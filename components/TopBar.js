332867c370b5d0a985e60f74414761aef89a2f28
Samuel Sackey <smlscky@gmail.com>
2021-11-03 21:56:53 +0000

    Add cart badges to topbar

--- components/TopBar.js
+++ components/TopBar.js
@@ -11,8 +11,14 @@ export default function TopBar() {
       <TouchableOpacity>
         <Feather name="settings" size={25} color="black" />
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate("Cart")}>
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Cart")}
+        style={{ flexDirection: "row" }}
+      >
         <FontAwesome name="opencart" size={24} color="black" />
+        <View style={styles.badgeContainer}>
+          <Text style={styles.badgeText}>3</Text>
+        </View>
       </TouchableOpacity>
     </View>
   );
@@ -25,5 +31,21 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     alignItems: "center",
     marginVertical: 10,
+  },
+  badgeContainer: {
+    backgroundColor: "#8c736d",
+    width: 20,
+    height: 20,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+    position: "relative",
+    right: 13,
+    top: 13,
+  },
+  badgeText: {
+    color: "white",
   },
 });
